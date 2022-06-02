d51f2a5f5170a89fc72e63629a40fec40633c51b
Ira <irinaturceaninov@gmail.com>
2022-06-02 22:37:35 +0300

    comit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 3;
+        int sum;
+        sum = number1 + number2;
+        System.out.println("This is a sum:" + sum);
+        System.out.printf("Sum is a %d%n", sum);
+
+    }
+}
