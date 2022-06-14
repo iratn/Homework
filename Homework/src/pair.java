d47cf6befbb9dc718f7d126023a8c2430f86b330
Ira <irinaturceaninov@gmail.com>
2022-06-14 18:52:06 +0300

    comit

---
+++ Homework/src/pair.java
@@ -0,0 +1,18 @@
+public class pair {
+
+        public static void main(String[] args) {
+
+
+            int number = 80;
+
+            System.out.print("Factors of " + number + " are: ");
+
+            for (int i = 1; i <= number; ++i) {
+
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
